b8ad0b9ff7c5c3df90afe5e8e8cbbd0a6644b5b9
Luciano655dev <lucianomenezes655@gmail.com>
2022-06-28 20:55:17 -0300

    marcadores adicionados

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Timer normal */
 let timerTxt = document.getElementById("timer")
 let pauseBtn = document.getElementById("pauseBtn")
 
@@ -30,7 +31,7 @@ function start(segP=0, minP=0, hrsP=0){
         if(min==3){
             pause()
         }
-        timerTxt.innerHTML = `${String(seg).padStart(2, '0')}:${String(min).padStart(2, '0')}:${String(hrs).padStart(2, '0')}`
+        timerTxt.innerHTML = `${String(hrs).padStart(2, '0')}:${String(min).padStart(2, '0')}:${String(seg).padStart(2, '0')}`
     }, 1000)
 }
 
@@ -50,4 +51,40 @@ function pause(){
         start(seg, min, hrs)
         isPaused=false
     }
+}
+
+/* Histórico do timer */
+let counter = document.getElementById('historicMain').childElementCount-1
+
+function mark(){
+    counter++
+
+    // Div config
+    let div = document.createElement('div')
+    div.classList.add('markDiv')
+    div.id = `mark${String(counter).padStart(2, '0')}`
+    document.getElementById('historicMain').appendChild(div)
+
+    // Text Config
+    let text = document.createElement('p')
+    text.innerHTML = `${String(counter).padStart(2, '0')} -> ${String(hrs).padStart(2, '0')}:${String(min).padStart(2, '0')}:${String(seg).padStart(2, '0')}`
+    document.getElementById(`mark${String(counter).padStart(2, '0')}`).appendChild(text)
+
+    // Delete Button Config
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'Delete'
+    deleteBtn.onclick = ()=>{
+        div.classList.add('markDivDelete')
+        setTimeout(()=>{
+            deleteBtn.parentElement.remove()
+            if((document.getElementById('historicMain').childElementCount) == 1){ counter=0 }
+        }, 300)
+
+    }
+    document.getElementById(`mark${String(counter).padStart(2, '0')}`).appendChild(deleteBtn)
+}
+
+function resetMark(){
+    counter = 0
+    document.getElementById('historicMain').innerHTML='<button class="resetMarkBtn" onclick="resetMark()">Reset</button>'
 }
